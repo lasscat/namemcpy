afd8d44f10475111e0ddf76763efba29b1f26537
Luke Lass <ants.uk.us@gmail.com>
2020-11-17 12:30:30 -0800

    Create setup.py

---
+++ pip/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="namemcpy",
+    version="1.0.0",
+    license="MIT",
+    author="Luke Lass",
+    author_email="ants.uk.us@gmail.com",
+    description="Api Wrapper for namemc",
+    long_description="Api Wrapper for https://namemc.com. This moudule is to make it easier to use namemcs api.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lasscat/namemcpy",
+    packages=setuptools.find_packages(),
+    install_requires = ["requests"],
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
+#https://packaging.python.org/tutorials/packaging-projects/
