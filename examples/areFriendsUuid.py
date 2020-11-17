9144a474854d523edd6ceace361afa12c78c59aa
Luke Lass <ants.uk.us@gmail.com>
2020-11-17 12:35:58 -0800

    usuage for areFriendsUuid

---
+++ examples/areFriendsUuid.py
@@ -0,0 +1,6 @@
+from namemcpy import namepy
+example = namepy()
+test = example.areFriendsUuid(uuid1='49a7bf33772045bc9f75712d15cdf544', uuid2='10fad8a9efe44713bf118ccc384de574')
+print(test)
+
+#output returns true or false
