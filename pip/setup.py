9514d26070c465eaafd5a342e6fcee109a0856f7
Luke Lass <ants.uk.us@gmail.com>
2020-11-18 09:28:43 -0800

    changed version!!

--- pip/setup.py
+++ pip/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="namemcpy",
-    version="1.0.0",
+    version="1.1.0",
     license="MIT",
     author="Luke Lass",
     author_email="ants.uk.us@gmail.com",
