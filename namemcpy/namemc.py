e4d46eeffdbfc10dc588e16e49fa0f1205c9e00d
Luke Lass <ants.uk.us@gmail.com>
2020-11-28 12:43:51 -0800

    updated bug

--- namemcpy/namemc.py
+++ namemcpy/namemc.py
@@ -1,3 +1,25 @@
+"""MIT License
+
+Copyright (c) 2020 Luke Lass
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 import time
 import requests
 import json
@@ -62,8 +84,7 @@ class namepy():
 
         if isinstance(username1, str) and isinstance(username2, str):
             # -------getting player 1 uuid-------
-            player_1_link = requests.get(
-                self.api_url + username1 + "?at=" + str(ts)).json()  # look at printfriendlist for what this means
+            player_1_link = requests.get(self.api_url + username1 + "?at=" + str(ts)).json()  # look at printfriendlist for what this means
             player_1_uuid = (player_1_link['id'])  # gets player 1 uuid value and stores it into the var
             # -------------------
             response_namemc = requests.get(self.friend_url + player_1_uuid + '/friends').json()
