93636d9b8f7d32e8bcb7a89347ee83e5bac75dd8
lasscat <ants.uk.us@gmail.com>
2020-12-14 08:46:12 -0800

    DONT USE THIS OLD DEF NAMES

--- examples/areFriendsUuid.py
+++ examples/areFriendsUuid.py
@@ -3,4 +3,4 @@ example = namepy()
 test = example.areFriendsUuid(uuid1='49a7bf33772045bc9f75712d15cdf544', uuid2='10fad8a9efe44713bf118ccc384de574')
 print(test)
 
-#output returns true or false
+#output returns true or fals
